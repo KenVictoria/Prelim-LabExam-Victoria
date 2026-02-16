fa48d7e052d04ec33d949b49cc51bd5b937560fd
kenneth victoria <kennethvictoria777@gmail.com>
2026-02-16 15:05:28 +0800

    Initial commit for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: 'Prelim-LabExam-Victoria' 
+  base: '/Prelim-LabExam-Victoria/', 
 })
